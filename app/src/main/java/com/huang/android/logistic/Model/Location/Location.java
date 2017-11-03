ba42e9dc2f3de1aa39f5ec50c2a67e66040794eb
David Wibisono <davidwibisono95@gmail.com>
2017-11-03 10:11:42 +0700

    update string

---
+++ app/src/main/java/com/huang/android/logistic/Model/Location/Location.java
@@ -0,0 +1,12 @@
+package com.huang.android.logistic.Model.Location;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by davidwibisono on 20/10/17.
+ */
+
+public class Location {
+    @SerializedName("name")
+    public String name;
+}
